0c4e8574095c2efdf2a0f1c71c72987c4ad6bff8
srividyapragada <126004644+srividyapragada@users.noreply.github.com>
2023-03-22 11:22:02 +0530

    assignment 2
    assignment 2

---
+++ program1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int f;
+int main()
+{
+	f=((1+2)*(3+4)*5);
+	printf("The value of the expression is :%d ",f);
+	return 0;
+}
---
+++ program2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	const int a=10;
+	printf("%d",a);
+	return 0;
+}
