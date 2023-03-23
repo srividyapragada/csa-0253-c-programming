adb6aa492ce026c735a9bdb8285e25b9053eb07c
srividyapragada <126004644+srividyapragada@users.noreply.github.com>
2023-03-23 09:06:17 +0530

    assignment-3
    assignment-3

---
+++ Assignment 3 program10.c
@@ -0,0 +1,10 @@
+int square(x)
+{
+	return x*x;
+}
+main()
+{
+int i;
+i = 64/square(4);
+printf("%d",i);
+}
---
+++ assignment 3 program 6.c
@@ -0,0 +1,11 @@
+//6. Predict the output or error(s) for the following program:
+#include<stdio.h>
+main()
+{
+	char string[]="Hello World";
+	display(string);
+}
+void display(string)
+{
+	printf("%s",string);
+}
